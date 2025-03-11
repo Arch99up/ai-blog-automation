d3ea307e26fbb9331c3cd480eb669d8973c56307
Arch99up <parcher1980@gmail.com>
2025-03-11 16:25:30 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Ensure the SQLite database and tables are created before starting the app
-python -c "from app import init_db; init_db()"
+python -c "import app; app.setup_database()"
 
 # Start the Flask application using Gunicorn
 gunicorn -b 0.0.0.0:10000 app:app
